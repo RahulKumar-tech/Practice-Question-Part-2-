e9bcf6a8e530469064a4f81d264449e014ac792b
RAHUL KUMAR <77164649+RahulKumar-tech@users.noreply.github.com>
2024-03-31 20:20:47 +0530

    Add files via upload

---
+++ c++AssignmentNo2.cpp
@@ -0,0 +1,102 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+// int x;
+// cout << "Enter first number\n";
+// cin >> x; // user will give ‘x’ a value.
+// int y, m;
+// cout << "Enter second number and value for taking modulus\n";
+// cin >> y >> m; // user will give ‘y’ a value.
+// int Z = (x * y) % m;
+// cout << "Output is: " << Z;
+// }
+//Output :-
+// Enter first number
+// 2
+// Enter second number and value for taking modulus
+// 3
+// 6
+// Output is: 0
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+// int x;
+// cout<<"Enter first number\n";
+// cin>>x; // user will give 'x' a value.
+// int y;
+// cout<<"Enter second number\n";
+// cin>>y; // user will give 'y' a value.
+// cout<<(x!=y)<<" "<<(x>=y);
+// }
+// Output:- 
+// Enter first number
+// 3
+// Enter second number
+// 2
+// 1 1
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+// int x,y;
+// cin>>x>>y;
+// x+=y;
+// x-=y;
+// x%=y;
+// cout<<x;
+// }
+// Output: -
+// 2
+// 3
+// 2
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float pi=3.14;
+//     int r;
+//     cout<<"Enter the radius : ";
+//     cin>>r;
+//     int h;
+//     cout<<"Enter the height : ";
+//     cin>>h;
+//     int volume = (int)pi;
+//     volume=pi*r*r*h;
+//     cout<<"volume of cylinder is : "<<volume;
+// }
+
+// Output:- 
+// Enter the radius : 20
+// Enter the height : 5
+// volume of cylinder is : 6280
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    char a,b;
+    cout<<"Enter 1st character : ";
+    cin>>a;
+    cout<<"Enter 2nd character : ";
+    cin>>b;
+    cout<<"The difference between ascii of two character is : "<<" "<<b-a<<endl;
+}
+
+//Output:-
+// Enter 1st character : a
+// Enter 2nd character : c
+// The difference between ascii of two character is :  2
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+// int i = ( 4 + 7 / 5 * 6 * 6+9 )% 100 ;
+// cout<<i;
+// }
+
+// Output:- 49
---
+++ realThings.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ using namespace std;
+int main(){
+    int a;
+    if(3+2%5){
+        cout<<"this works";
+    }
+    if(a = 10){
+        cout<<"even this also works";
+    }
+    if(-9){
+        cout<<"Surprisingly even this also works";
+    }
+}
